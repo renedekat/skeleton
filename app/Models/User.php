84ccd50a700a77bbba7d0de21c6077615abcfb20
René de Kat <renedekat@Renes-MacBook-Pro-2.local>
2024-08-06 14:23:04 +0100

    Added pennant feature flags and enabled must verify email

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,19 +2,21 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Jetstream\HasTeams;
+use Laravel\Pennant\Concerns\HasFeatures;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
+    use HasFeatures;
     use HasProfilePhoto;
     use HasTeams;
     use Notifiable;
